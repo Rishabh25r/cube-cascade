815a2e714b1ac83312871f900e07f98d5acc77c9
Rishabh Rokariya <128992766+Rishabh25r@users.noreply.github.com>
2023-12-05 02:30:11 +0530

    Add files via upload

---
+++ game/script.js
@@ -0,0 +1,33 @@
+
+alert("Lets start the game");
+var maxscore=prompt("Enter maximum desured score");
+function rollDice() {
+    
+    // Get random numbers for the two dice
+    const number1 = Math.floor(Math.random() * 6) + 1;
+    const number2 = Math.floor(Math.random() * 6) + 1;
+
+    // Update the dice visuals
+    document.getElementById("dice1").innerText = number1;
+    document.getElementById("dice2").innerText = number2;
+
+    // Check if the numbers are equal
+    if (number1 === number2) {
+        // Update the score if the numbers are equal
+        updateScore(number1);
+    }
+}
+
+let score = 0;
+
+function updateScore(increaseBy) {
+    // Increase the score by the given amount
+    score += increaseBy;
+
+    // Update the score display
+    document.getElementById("result").innerText = "Score: " + score;
+    if(score>=maxscore){
+    alert("Your have successfully finished the game");
+   return 0;
+    }
+}
